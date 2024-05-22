370a91a00d47e54b0bcca30cffc3e71abacaedfb
KhanRana <47982799+KhanRana@users.noreply.github.com>
2024-05-22 16:15:16 +0100

    create api to parse the header

--- index.js
+++ index.js
@@ -21,7 +21,15 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
+  console.log(req.headers.host);
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress:req.headers.host,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+    });
 });
 
 // listen for requests :)
